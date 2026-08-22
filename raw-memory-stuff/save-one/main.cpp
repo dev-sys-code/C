5e02e888450a9b6b0d0da8a4af86779bd6517595
Kayden <Kaydenbuster@outlook.com>
2026-08-22 19:21:39 +0100

    Update main.cpp

--- raw-memory-stuff/save-one/main.cpp
+++ raw-memory-stuff/save-one/main.cpp
@@ -13,6 +13,7 @@ struct PacketHeader {
 int main() {
     uint8_t rawMemory[8]{0};
     PacketHeader* header = reinterpret_cast<PacketHeader*>(rawMemory);
+    
     header->id = 0x1337;
     header->status = 0x0001;
     header->payload = 42069;
